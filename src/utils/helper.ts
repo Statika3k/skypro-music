9cb73acbc0eb7d6da831e42806f43ef1704bff37
Nina Shakhanova <shahanova.ns@gmail.com>
2026-02-14 10:17:30 +0300

    Добавлена функция для создания годов из фильтрации треков

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -7,20 +7,20 @@ export function getUniqueValuesByKey(
   // Используем set для хранения уникальных значений
   const uniqueValues = new Set<string>();
 
-   //проходим по каждому объекту в массиве 
+  //проходим по каждому объекту в массиве
   arr.forEach((item) => {
     const value = item[key];
 
     //Если значение - мвссив строк
     if (Array.isArray(value)) {
       value.forEach((v) => {
-        if (v) {
+        if (v && typeof v === 'string') {
           uniqueValues.add(v);
         }
       });
-    } 
+    }
     //Если значение - строка
-    else if (typeof value === 'string') {
+    else if (typeof value === 'string' && value) {
       uniqueValues.add(value);
     }
   });
@@ -36,3 +36,20 @@ export function formatTime(time: number) {
 
   return `${minutes}:${outputSeconds}`;
 }
+
+export function getUniqueYears(tracks: TrackType[]): string[] {
+  const years = tracks.map(track => {
+    if (track.release_date) {
+      const year = track.release_date.split('-')[0];
+      return `${year} год`;
+    }
+    return '';
+  }).filter(year => year !== '');
+  
+  return [...new Set(years)].sort((a, b) => {
+    const yearA = parseInt(a.split(' ')[0]);
+    const yearB = parseInt(b.split(' ')[0]);
+    return yearB - yearA;
+  });
+}
+  
