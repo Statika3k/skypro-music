6e9693900b993a785f32ac38dd587eec3da70ffe
Nina Shakhanova <shahanova.ns@gmail.com>
2026-03-22 21:18:40 +0300

    Главная страница перенесена в src/app/music/main с общим лайаутом

---
+++ src/app/music/layout.tsx
@@ -0,0 +1,27 @@
+import Bar from '@/components/Bar/Bar';
+import Navigation from '@/components/Navigation/Navigation';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import { ReactNode } from 'react';
+import styles from './layout.module.css';
+
+interface AuthLayoutProps {
+  children: ReactNode;
+}
+
+export default function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <Navigation />
+            {children}
+            <Sidebar />
+          </main>
+          <Bar />
+          <footer className="footer"></footer>
+        </div>
+      </div>
+    </>
+  );
+}
