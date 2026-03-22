30ce0427b93f785b30bfc93c02c031ee82245cf9
Nina Shakhanova <shahanova.ns@gmail.com>
2026-03-22 21:17:34 +0300

    Созданы функции для работы с музыкой

---
+++ src/services/tracks/tracksApi.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { BASE_URL } from "../constants"
+import { PlayListType, TrackType } from "@/sharedTypes/sharedTypes"
+
+export const getAllTracks = (): Promise<TrackType[]> => {
+    return axios(BASE_URL + '/catalog/track/all/').then((res) => {
+        return res.data.data
+    })
+}
+
+export const getTracksSelection = (id: string): Promise<PlayListType> => {
+    return axios(BASE_URL + `/catalog/selection/${id}/`).then((res) => {
+        return res.data.data
+    })
+}
+
+export const getTrackById = (id: string): Promise<TrackType> => {
+    return axios.get(BASE_URL + `/catalog/track/${id}/`).then((res) => res.data.data)
+}
