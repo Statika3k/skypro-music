1a36336d6fe93ee9f98d1f71a27852fd135ff40f
Nina Shakhanova <shahanova.ns@gmail.com>
2026-03-30 18:51:42 +0300

    Добавлены функции для работы с избранными треками

--- src/services/tracks/tracksApi.ts
+++ src/services/tracks/tracksApi.ts
@@ -1,15 +1,36 @@
-import axios from "axios"
-import { BASE_URL } from "../constants"
-import { PlayListType, TrackType } from "@/sharedTypes/sharedTypes"
+import axios from 'axios';
+import { BASE_URL } from '../constants';
+import { PlayListType, TrackType } from '@/sharedTypes/sharedTypes';
+
 
 export const getAllTracks = (): Promise<TrackType[]> => {
-    return axios(BASE_URL + '/catalog/track/all/').then((res) => {
-        return res.data.data
-    })
-}
+  return axios(BASE_URL + '/catalog/track/all/').then((res) => {
+    return res.data.data;
+  });
+};
 
 export const getTracksSelection = (id: string): Promise<PlayListType> => {
-    return axios(BASE_URL + `/catalog/selection/${id}/`).then((res) => {
-        return res.data.data
-    })
-}
+  return axios(BASE_URL + `/catalog/selection/${id}/`).then((res) => {
+    return res.data.data;
+  });
+};
+
+export const addLike = (access: string, id: number) => {
+  return axios.post(
+    BASE_URL + `/catalog/track/${id}/favorite/`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    },
+  );
+};
+
+export const removeLike = (access: string, id: number) => {
+  return axios.delete(BASE_URL + `/catalog/track/${id}/favorite/`, {
+    headers: {
+      Authorization: `Bearer ${access}`,
+    },
+  });
+};
