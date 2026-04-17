b00efe503d73eaea37821db6f76e77680ee35ee4
Nina Shakhanova <shahanova.ns@gmail.com>
2026-04-17 20:01:05 +0300

    Сделаны изменения для лайкнутых треков

--- src/app/music/favorite/page.tsx
+++ src/app/music/favorite/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import Centerblock from '@/components/Centerblock/Centerblock';
-import { useAppSelector } from '@/store/store';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { resetFilters } from '@store/features/trackSlice';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function FavoritePage() {
   const router = useRouter();
+  const dispatch = useAppDispatch();
   const { favoriteTracks, fetchIsLoading, fetchError } = useAppSelector(
     (state) => state.tracks,
   );
   const { access } = useAppSelector((state) => state.auth);
 
   const isChecking = fetchIsLoading || !access;
+
+  useEffect(() => {
+    dispatch(resetFilters());
+  }, [dispatch]);
 
   useEffect(() => {
     if (!fetchIsLoading && !access) {
